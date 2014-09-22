ac7f7f33bcb07a88960e32325333d0facf652e70
Jakub Sikorski <jakubsikorski7@gmail.com>
2014-09-22 23:03:23 +0200

    fix: query for user task lists

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -97,7 +97,7 @@ exports.me = function(req, res, next) {
 
 exports.showTaskLists = function(req, res, next) {
   var userId = req.params.id;
-  TaskList.find({}, function (err, taskLists) {
+  TaskList.find({ userIds: userId }, function (err, taskLists) {
     if(err) { return handleError(res, err); }
     return res.json(200, taskLists);
   });
