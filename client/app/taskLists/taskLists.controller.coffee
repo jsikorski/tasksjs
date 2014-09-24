5addd9cb7e9ce0808a81e8f759845ca14d1c8eda
Jakub Sikorski <jakubsikorski7@gmail.com>
2014-09-24 02:20:39 +0200

    feature: show current version in footer

--- client/app/taskLists/taskLists.controller.coffee
+++ client/app/taskLists/taskLists.controller.coffee
@@ -35,6 +35,9 @@ tasksjsApp.controller 'TaskListsCtrl', ($scope, $http, socket, $modal, Auth, Use
   $scope.$on '$destroy', ->
     socket.unsyncUpdates 'task-list'
 
+  $http.get('/version').then (response) ->
+    $scope.version = response.data
+
 
 
 tasksjsApp.controller 'TaskListCtrl', ($scope, $modal, $state) ->
--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,8 @@
 'use strict';
 
 var errors = require('./components/errors');
+var config = require('./config/environment');
+var version = require(config.root + '/package.json').version;
 
 module.exports = function(app) {
 
@@ -14,6 +16,10 @@ module.exports = function(app) {
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
+
+  app.get('/version', function(req, res) {
+    res.send(version);
+  });
   
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
