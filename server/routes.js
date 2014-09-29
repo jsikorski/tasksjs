6bb37db13bd0a985c3f6253dfb03e94b935794ff
Jakub Sikorski <jakubsikorski7@gmail.com>
2014-09-29 02:14:02 +0200

    chore: removed scaffold awesomeThings api

--- server/routes.js
+++ server/routes.js
@@ -12,7 +12,6 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/task-lists', require('./api/task-list'));
-  app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
